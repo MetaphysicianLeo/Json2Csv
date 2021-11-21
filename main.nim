19a4a49cf0241124e908c2d422c7b1cdcfbb7825
Αλταιρ <33951689+MetaphysicianLeo@users.noreply.github.com>
2021-11-21 11:45:02 +0000

    file read

--- main.nim
+++ main.nim
@@ -1,1 +1,33 @@
-echo "hello world!"
+import json
+
+template withOpen(
+    file: untyped,
+    filename: string,
+    mode: FileMode,
+    statements: untyped) =
+    block:
+        var file: File
+        if open(file, filename, mode):
+            try:
+                statements
+            finally:
+                file.close()
+        else:
+            quit("cannot open: " & filename)
+    
+
+
+# let s = ( proc (x: File) : string =
+#    result = x.readAll
+#    x.close
+# ) open("input.json", fmRead) 
+# let jsonNode = parsejson(s)
+
+var s: string
+withOpen(f, "input.json", fmRead):
+    s = f.readAll
+
+let content = parseJson(s)
+
+for x in content:
+    echo x.kind
